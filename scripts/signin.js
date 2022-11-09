0fd114ed8d352c1b94a113902f99a2b106f0b975
harishpampana7 <harishpampana7675@gmail.com>
2022-11-09 21:57:01 +0530

    the

---
+++ scripts/signin.js
@@ -0,0 +1,22 @@
+
+function submit(){
+    let email1=document.querySelector("#HP_name").value
+    let password1=document.querySelector("#HP_password").value
+    
+    let data=JSON.parse(localStorage.getItem("HP_user"))
+    
+    if(data.email===email1 && data.password===password1){
+        alert("Sigin succesfully")
+        window.location.href="index.html"
+    }
+    else{
+        let alert1=document.getElementById("alert")
+        alert1.innerText="Sorry, we don't recognize that email. Please try again."
+        alert("Sorry, we don't recognize that email. Please try again.")
+    }
+}
+
+
+let nav_logo = document.getElementById("nav_logo").addEventListener("click",function(){
+    window.location.href ="index.html"
+})
---
+++ scripts/signup.js
@@ -0,0 +1,26 @@
+
+
+submit=()=>{
+
+    function Obj(text,email,password){
+    this.name=text;
+    this.email=email;
+    this.password=password;
+    
+    }
+    
+    let text=document.querySelector("#HP_name").value;
+    let email=document.querySelector("#HP_email").value
+    let password=document.querySelector("#HP_password").value
+    
+    let HP_Data=new Obj(text,email,password)
+    console.log(HP_Data)
+    
+    localStorage.setItem("HP_user",JSON.stringify(HP_Data))
+    window.location.href="signin.html"
+    }
+   
+    let nav_logo = document.getElementById("nav_logo").addEventListener("click",function(){
+        window.location.href ="index.html"
+    })
+    
