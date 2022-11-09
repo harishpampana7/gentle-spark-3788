2b1fa077688ad465cb8baf2721e9470b263548d9
sohel <mr.sohelsyed@gmail.com>
2022-11-09 21:33:54 +0530

    done day-1 work

---
+++ index.js
@@ -0,0 +1,40 @@
+let api = `  http://localhost:3000/hotel`;
+
+window.onload = () => {
+  let addBtn = document.getElementById("submitBtn");
+  addBtn.onclick = () => {
+    addPlace(event);
+  };
+};
+
+let addPlace = async (e) => {
+  e.preventDefault();
+
+  let form = document.getElementById("form-data");
+
+  let image = form.image.value;
+  let name = form.name.value;
+  let type = form.type.value;
+  let price = +form.price.value;
+  let rating = +form.rating.value;
+  let city = form.city.value;
+
+  let data = {
+    id: Date.now() + Math.random() * 100,
+    name,
+    image,
+    price,
+    type,
+    rating,
+    city,
+  };
+  console.log(data);
+
+  await fetch(api, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
