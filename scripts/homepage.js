2c245357440096333b48d8a1e438f179008eee57
Shubham Kumar <shubhkmr1806@gmail.com>
2022-11-10 22:21:21 +0530

    day-3

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,29 +1,41 @@
-// let api = "https://test.api.amadeus.com/v1/reference-data/locations/hotels/by-city"
 
-let imagesArr = [
-    "https://drive.google.com/uc?export=view&id=1JsJIUh36pLiiMkSMyLjIfYU8d-PdWe5z",
-    "https://drive.google.com/uc?export=view&id=1Jf9WtG0VQgpdNrfxawGwJBpzVAuAgCW5",
-    "https://drive.google.com/uc?export=view&id=1f3NGuMZlCA7xg2cSv3GpJ2W4rb9rcN9x",
-    "https://drive.google.com/uc?export=view&id=1JmQo74orm_9qr3c3B1vYEZ-ihRC2myTL",
-    "https://drive.google.com/uc?export=view&id=16j-2lK0BtdLNW4Hqf8Wrc3iU8ywNs3u_",
-]
 
-let container = document.getElementById("homevideo")
 
-let index = 0;
-let start = setInterval(() => {
-    
-    if(index===imagesArr.length){
-        index = 0
-    }
-    container.innerHTML = null;
-    let image = document.createElement("img")
-    image.src = imagesArr[index];
-    image.id = "gif"
-    // document.getElementById('gif').className += "fadeOut"
-    container.append(image)
-    index++
-},6000);
+//slideshow
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 6000);    
+}
+
+document.getElementById("SKlogo").addEventListener("click",function(){
+    window.location.href = "index.html"
+})
+
+//cities swiper;
+let slidesContainerA = document.getElementById("slides-containerA");
+let slideA = document.querySelector(".slideA");
+let prevButtonA = document.getElementById("slide-arrow-prevA");
+let nextButtonA = document.getElementById("slide-arrow-nextA");
+
+nextButtonA.addEventListener("click", () => {
+  let slideWidth = slideA.clientWidth;
+  slidesContainerA.scrollLeft += slideWidth;
+});
+
+prevButtonA.addEventListener("click", () => {
+  let slideWidth = slideA.clientWidth;
+  slidesContainerA.scrollLeft -= slideWidth;
+});
 
 
 
