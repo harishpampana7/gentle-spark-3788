2fcf33ffd079c1acbd22d81752e35994e3052be1
harishpampana7 <harishpampana7675@gmail.com>
2022-11-11 17:27:54 +0530

    the end

---
+++ components/footer.js
@@ -0,0 +1,96 @@
+
+const footer=()=> {
+    return `<div id="HP_about">
+                <p>About BOOKit.com</p>
+                <p>Terms & Conditions</p>
+                <p>How We Work</p>
+                <p>Privacy & Cookie statement</p>
+                <p>Flights Help</p>
+            </div>
+
+        <hr>
+
+            <div id="HP_all_links">
+
+                <div id="HP_links_container1">
+                    <div>
+                        <h4>Help</h4>
+                        <h4>About us</h4>
+                        <h4>Contact us</h4>
+                        <h4>Warranty</h4>
+                        <h4>FAQs</h4>
+                        <h4>Sitemap</h4>
+                    </div>
+
+                    <div>
+                        <h4>Support</h4>
+                        <h4>Payment information</h4>
+                        <h4>Wholesale</h4>
+                        <h4>Blog</h4>
+                        <h4>Affiliates</h4>
+                    </div>
+
+                    <div>
+                        <h4>Policies</h4>
+                        <h4>Terms & conditions</h4>
+                        <h4>Return Policy</h4>
+                        <h4>Privacy</h4>
+                        <h4>Declaration</h4>
+                        <h4>Custom</h4>
+                    </div>
+                </div>
+
+                <div id="HP_links_container2">
+                    <h4>Download the App</h4>
+                    <div>
+                        
+                        <div id="HP_qr_images">
+                            <div>
+                                <img src="https://content1.geekbuying.com/V1.4/en/images/index_images/android_app.png" alt="">
+                            </div>
+
+                            <div>
+                                <img src="https://content1.geekbuying.com/V1.4/en/images/index_images/app_store.jpg" alt="">
+                                <img src="https://content1.geekbuying.com/V1.4/en/images/index_images/google_play.jpg" alt="">
+                                <img src="https://content1.geekbuying.com/V1.4/en/images/index_images/gallery.jpg" alt="">
+                            </div>
+                            
+                        </div>
+                        <div id="HP_important_links">
+                            <div><img class="HP_links_icon" src="https://cdn-icons-png.flaticon.com/128/893/893257.png" alt=""><span>Service_payment@BOOKit.com</span> </div>
+                            <div><img class="HP_links_icon" src="https://cdn-icons-png.flaticon.com/128/7937/7937770.png" alt=""><span>Ticket</span> </div>
+                            <div><img class="HP_links_icon" src="https://cdn-icons-png.flaticon.com/128/3237/3237472.png" alt=""><span>Live Chat</span> </div>
+                        </div>
+                 </div>
+
+                <h4>Connect with us</h4>
+                <div id="HP_social_links">
+                    <img class="HP_links_icon" src="https://cdn-icons-png.flaticon.com/128/3128/3128304.png" alt="">
+                    <img class="HP_links_icon" src="https://cdn-icons-png.flaticon.com/128/6681/6681032.png" alt="">
+                    <img class="HP_links_icon" src="https://cdn-icons-png.flaticon.com/128/786/786407.png" alt="">
+                    <img class="HP_links_icon" src="https://cdn-icons-png.flaticon.com/128/1057/1057248.png" alt="">
+                    <img class="HP_links_icon" src="https://cdn-icons-png.flaticon.com/128/733/733579.png" alt="">
+                    <img class="HP_links_icon" src="https://cdn-icons-png.flaticon.com/128/1724/1724681.png" alt="">
+                </div>
+        
+            </div>
+    
+        </div>
+
+<hr>
+
+                <div id="HP_copyright">
+                    <p>Copyright © 1996-2022 Bookit.com. All rights reserved.</p>
+                    <p>Bookit.com is part of Booking Holdings Inc., the world leader in online travel and related services.</p>
+                </div>
+
+                <div id="HP_footer_img_log">
+                    <img src="https://q-cf.bstatic.com/static/img/tfl/group_logos/logo_priceline/f80e129541f2a952d470df2447373390f3dd4e44.png" alt="">
+                    <img src="https://r-cf.bstatic.com/static/img/tfl/group_logos/logo_kayak/83ef7122074473a6566094e957ff834badb58ce6.png" alt="">
+                    <img src="https://q-cf.bstatic.com/static/img/tfl/group_logos/logo_agoda/1c9191b6a3651bf030e41e99a153b64f449845ed.png" alt="">
+                    <img src="https://r-cf.bstatic.com/static/img/tfl/group_logos/logo_rentalcars/6bc5ec89d870111592a378bbe7a2086f0b01abc4.png" alt="">
+                    <img src="https://r-cf.bstatic.com/static/img/tfl/group_logos/logo_opentable/a4b50503eda6c15773d6e61c238230eb42fb050d.png" alt="">
+                </div>`
+}
+
+export default footer
